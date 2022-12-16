7ee2316f84f7a5c10692fecd828e216480fff29a
dio <bioroobo@gmail.com>
2022-12-16 11:57:46 +0000

    One to many relationships

--- firstapp/models.py
+++ firstapp/models.py
@@ -5,3 +5,13 @@ class Person(models.Model):
     age = models.IntegerField()
     objects = models.Manager()
     DoesNotExist = models.Manager # на работу не влияет, чтобы pycharm не выдавало ошибку
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Product(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE) # CASCADE, PROTECT, SET_NULL, SET_DEFAULT, DO_NOTHING
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+
